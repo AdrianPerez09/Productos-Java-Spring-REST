6980d1a2033a3e2bebeeca733867618018b0e15a
AdrianPerez09 <adriliko28@gmail.com>
2026-06-04 13:58:46 +0200

    30/05/2026 15:57:54 CEST
    -------------------------------
               CHANGES:
    - New Entity Controllers
    
    --- Creado por Adrián Pérez ---

---
+++ src/main/java/com/adrianEjemploSpringRest/apirest_productos/controllers/BrandController.java
@@ -0,0 +1,27 @@
+package com.adrianEjemploSpringRest.apirest_productos.controllers;
+
+import com.adrianEjemploSpringRest.apirest_productos.entities.Brand;
+import com.adrianEjemploSpringRest.apirest_productos.services.BrandServiceImp;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/brands")
+public class BrandController {
+
+    // Servicio de marcas
+    private final BrandServiceImp brandService;
+
+    // Obtener todas las marcas
+    @GetMapping
+    public List<Brand> getAllBrands() {
+
+        return brandService.findAll();
+
+    }
+}
